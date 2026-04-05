974a3e56bbca5e32d97823ea11d96e30b0815d91
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-05 18:31:53 +0530

    Learnt about Python  Module

---
+++ 02variable.py
@@ -0,0 +1,10 @@
+a = 1 # variable = container for storing data values
+
+b = 2 # identifier = name of variable class/function/module/package (e.g., a, b, c, my_function, etc.)
+
+c ="Harry Potter" # string variable = sequence of characters enclosed in quotes("")
+# keywords = reserved words in Python that cannot be used as variable names (e.g., if, else, for, while, etc.)
+d = 3.14 # float variable = number with decimal point
+
+print(a+b)
+print(c)
---
+++ 03datatype.py
@@ -0,0 +1,33 @@
+a = 1 # a is an integer variable
+
+b = 5.22 # b is a float variable
+
+c ="Jiya" # c is a string variable
+
+d = False # d is a boolean variable
+
+e = None # e is a NoneType variable
+#Rules for variable names:
+#1. Variable names must start with a letter (a-z, A-Z) or an uderscore (_).
+#2. Variable names cannot start with a number (0-9).
+#3. Variable names can only contain letters, numbers, and underscores (a-z, A-Z, 0-9, _).
+#4. Variable names are case-sensitive (e.g., myVariable and myvariable are different).
+#5. Variable names cannot be a reserved keyword in Python (e.g., if, else, for, while, etc.).
+
+#exmple of valid variable names
+my_variable = 10
+_variable2 = "Hello"
+variable_3 = 3.14
+#example of invalid variable names
+#1. 2variable = 20 (starts with a number)
+#2. my-variable = "Hi" (contains a hyphen)
+#3. for = 5 (reserved keyword)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(my_variable)
+print(_variable2)
+print(variable_3)
