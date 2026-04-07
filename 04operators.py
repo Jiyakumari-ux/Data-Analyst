4b8a89ae305d8d113cbe7f86b3e71b41d0c47078
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-07 21:59:49 +0530

    arithmetic

---
+++ 04operators.py
@@ -0,0 +1,60 @@
+# Arithmetic operators
+a = 10
+b = 5
+c= a + b # addition
+d = a - b # subtraction
+e = a * b # multiplication
+f = a / b # division
+g = a % b # modulus
+h = a ** b # exponentiation
+i = a // b # floor division
+print("Addition:", c)
+print("Subtraction:", d)
+print("Multiplication:", e)
+print("Division:", f)
+print("Modulus:", g)
+print("Exponentiation:", h)
+print("Floor Division:", i)
+# Assignment operators
+x = 10; b = 5; c = 2; d = 8; e = 15; f = 3; g = 20
+x += 5 # x = x + 5
+b -= 3 # x = x - 3
+c *= 2 # x = x * 2
+d /= 4 # x = x / 4
+e %= 3 # x = x % 3
+f **= 2 # x = x ** 2
+g //= 2 # x = x // 2
+print("Final value of x:", x)
+print("Final value of b:", b)
+print("Final value of c:", c)
+print("Final value of d:", d)
+print("Final value of e:", e)
+print("Final value of f:", f)
+print("Final value of g:", g)
+# Comparison operators (always give boolean output)
+d = 5<4 # less than
+e = 5>4 # greater than
+f = 5<=5 # less than or equal to
+g = 5>=6 # greater than or equal to
+h = 5==5 # equal to
+i = 5!=4 # not equal to
+print("5 < 4:", d)
+print("5 > 4:", e)
+print("5 <= 5:", f)
+print("5 >= 6:", g)
+print("5 == 5:", h)
+print("5 != 4:", i)
+# Logical operators (always give boolean output)
+e = True or False # logical OR
+print("True or False:", True or False)
+print("True or False:", True or True) 
+print("False or True:", False or True) 
+print("False or False:", False or False)
+f = True and False # logical AND
+print("True and False:", True and False)
+print("True and True:", True and True)
+print("False and True:", False and True)
+print("False and False:", False and False)
+g = not True # logical NOT
+print("Not True:", not True)
+print("Not False:", not False)
