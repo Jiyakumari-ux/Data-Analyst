2c71a59b1190050bd4881da0cbc6139b9d6f10ea
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-09 22:08:36 +0530

    Joke implementation

---
+++ Python-Practice/jokes.py
@@ -0,0 +1,11 @@
+# import pyjokes
+
+# # print("printing jokes")  # ctrl+/ : comment out the line
+# joke = pyjokes.get_joke()
+# print(joke)
+import pyjokes
+
+print("printing joke :")
+joke = pyjokes.get_joke() #get_joke for single joke each time 
+print(joke)      #get_jokes for multiple jokes at a time
+  #get_jokes(n=5) for specifice no. of jokes at a time
