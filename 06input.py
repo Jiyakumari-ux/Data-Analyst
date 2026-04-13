a6ae3492d32df61b5ff27afdba92ff699aa5d8ae
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-13 23:08:55 +0530

    Input assigning

---
+++ 06input.py
@@ -0,0 +1,14 @@
+a = input("Enter number 1 : ")
+
+b = input("Enter number 2 : ")
+
+print("Number a is: ", a)
+print("Nuber b is: ", b)
+print("Sum is: ", a + b) #concatenate ho jate h + krne se strings kyuki a aur b string hai 
+
+
+c = int(input("Enter number 1 : "))
+d = int(input("Enter number 2 : "))
+print("Number c is: ", c)
+print("Number d is: ", d)
+print("Sum is: ", c + d) #abhi c aur d int type ke hai to + krne se sum aayega
