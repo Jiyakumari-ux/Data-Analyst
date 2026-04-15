dc19f3b4518a9113948d7ffee1733fb4ce19ca33
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-15 22:06:43 +0530

    solved tuple quetion

--- Python-Practice/problems/problem18.py
+++ Python-Practice/problems/problem18.py
@@ -1,1 +1,4 @@
 #check that a type cannot be changed in python
+a = (34, 234, "Harry")
+
+a [2] = "Larry" # This will give an error because tuples are immutable in Python, meaning their elements cannot be changed after they are created.
--- Python-Practice/problems/problem19.py
+++ Python-Practice/problems/problem19.py
@@ -1,2 +1,4 @@
 #wap to sum a list with 4 numbers
 number = [1, 2, 3, 4]
+
+print(sum(number))
