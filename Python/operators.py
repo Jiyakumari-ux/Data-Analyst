f59153740b43d3928d5e6a93e740cbc548020890
Jiya Kumari <jiyakittu2018@gmail.com>
2026-04-27 16:09:11 +0530

    operators

---
+++ Python/operators.py
@@ -0,0 +1,36 @@
+#arithmetic operator
+a = 7
+b = 12
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+#relational/comparison operator
+c = 23.5
+d = 56
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+#logical operator
+e = False
+f = True
+print(not(c>d))
+print(e and f)
+print(e or f)
+#assignment operator
+num =10
+num =+ 10
+print(num)
+num =-5
+print(num)
+num /= 5
+print(num)
+num *= 2
+print(num)
+num **= 3
+print(num)
