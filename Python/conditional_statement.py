7cb2ea2d8a7773bfaec3de634f5dca6f03d79d76
Jiya Kumari <jiyakittu2018@gmail.com>
2026-05-06 22:40:25 +0530

    conditional_statement concept

---
+++ Python/conditional_statement.py
@@ -0,0 +1,13 @@
+#if-elif-else statement
+condition1 = True
+condition2 = False
+
+if (condition1):
+    print("condition 1 is true")
+    #code to be executed if condition is true
+elif (condition2):
+    print("condition 2 is true")
+    #code to be executed if condition is true
+else:
+    print("all conditions are false")
+    #code to be executed if all conditions are false
